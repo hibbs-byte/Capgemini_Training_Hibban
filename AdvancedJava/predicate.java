f902d81290092ed90be78521c1dea810fc2ee4a9
Mohammad Hibban Ali <alihibban78@gmail.com>
2026-02-27 11:33:42 +0530

    Create predicate.java

---
+++ AdvancedJava/predicate.java
@@ -0,0 +1,19 @@
+package basic_interface;
+
+
+import java.util.function.Predicate;
+
+public class Predicateexample {
+       public static void main(String[] args)
+       {
+           Predicate<Integer> predicate=new Predicate<Integer>() {
+               @Override
+               public boolean test(Integer integer) {
+                   return integer>=11;
+               }
+           };
+           System.out.println(predicate.test(10));
+           Predicate<Integer> predicate1=number->number>0;
+           System.out.println(predicate1.test(10));
+       }
+}
