d0e69d63ce81d8913c133fa092035a0fd97664dd
Mohammad Hibban Ali <alihibban78@gmail.com>
2026-02-28 09:32:09 +0530

    Create readdemo.java

---
+++ AdvancedJava/FILE HANDLING/readdemo.java
@@ -0,0 +1,23 @@
+package read_file;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+
+public class readDemo {
+    public static void main(String[] args)
+    {
+        try
+        {
+            BufferedReader reader=new BufferedReader(new FileReader("data/students.txt"));
+            String line;
+            while((line= reader.readLine())!=null)
+            {
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
